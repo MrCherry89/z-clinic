8eb775c7798c450d39c13512ceb00e12879d011f
MrCherry89 <abgarnati@yandex.ru>
2023-06-01 07:21:16 +0400

    sdelal polovinu pravok

--- js/script.js
+++ js/script.js
@@ -192,6 +192,16 @@ $(document).ready(function () {
     ],
   });
 
+  $(".youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false,
+  });
+
   $(".unique-slider5").slick({
     dots: false,
     infinite: true,
